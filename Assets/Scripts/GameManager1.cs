b0b6f0f56c5425dc4ddb9d3c2c71991b55db9599
Wuming77 <2313432829@qq.com>
2021-07-27 01:41:22 +0800

    origin/main

---
+++ Assets/Scripts/GameManager1.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager1 : MonoBehaviour
+{
+    private float spawnRate = 1.0f;
+    public List<GameObject> targets;
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    public Button restartButton;
+    public GameObject titleScreen;
+    public Camera camera1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "Score: " + score;
+        //Debug.Log(targets.Count);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+    public void GameOver()
+    {
+        isGameActive = false;
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        //score = 0;
+        spawnRate /= difficulty;
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/TimerTrick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Timers;
+
+public class TimerTrick : MonoBehaviour
+{
+    public float time = 60;
+    public TextMeshProUGUI scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        time = time - Time.deltaTime;
+        //float time1 = Mathf.Round(time);
+        scoreText.text = "Time: " + Mathf.Round(time);
+    }
+}
